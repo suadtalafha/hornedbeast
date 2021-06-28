2b7a6a27b9aeffd12203bd015a5d81000894e62c
suadtalafha <suadhusam@yahoo.com>
2021-06-28 17:10:53 +0300

    save

--- src/component/Hornedbeast.js
+++ src/component/Hornedbeast.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 
 class Hornedbeast extends React.Component{
-    render(){
-        return(
-
-            <div>
-                <h2>
-                {this.props.title}
-                </h2>
-                <img src={this.props.url} alt={this.props.alt}/>
-                
-                <p>
-{this.props.pragraph}
-                </p>
-            </div>
-        )
-    }
+   constructor(props){
+       super(props);
+       this.state={
+           numberOflikes:0
+       }
+   }
+   increseNoflikes =()=>{
+       this.setState({
+        numberOflikes : this.state.numberOflikes +1
+       })
+   }
+   render(){
+       return(
+        <Card style={{ width: '18rem' }}>
+        <Card.Img onClick={this.increseNoflikes} variant="top" src={this.props.url} alt={this.props.title} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+       {this.props.pargraph}
+          </Card.Text>
+          <Card.Text>
+              Num of likes : {this.state.numberOflikes}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+       )
+   }
 }
 
 export default Hornedbeast;
